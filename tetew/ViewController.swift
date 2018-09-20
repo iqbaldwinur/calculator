ac01395047403ebb2bb68142b34d35dd1116aa9d
Iqbal Dwi Nur Khoirul anam <iqbaldwinurkhoirulanam@tetew.local>
2018-09-20 13:58:15 +0700

    Initial Commit

---
+++ tetew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tetew
+//
+//  Created by Iqbal Dwi Nur Khoirul anam on 20/09/18.
+//  Copyright © 2018 Iqbal Dwi Nur Khoirul anam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
